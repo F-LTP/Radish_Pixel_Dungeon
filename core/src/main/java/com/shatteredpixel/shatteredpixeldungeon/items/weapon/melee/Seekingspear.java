56f04180e9668acda43ce8df19a931ecaec40204
Budding <1426725846@qq.com>
2023-06-14 20:50:46 +0800

    v0.0.3: rebalance Seekingspear

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Seekingspear.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Seekingspear.java
@@ -24,7 +24,7 @@ public class Seekingspear extends MeleeWeapon{
     @Override
     public int max(int lvl) {
         return  4*(tier+1) +    //20 base, down from 25
-                lvl*(tier+1);   //scaling unchanged
+                lvl*(tier);   //scaling down
     }
     @Override
     public ItemSprite.Glowing glowing() {
