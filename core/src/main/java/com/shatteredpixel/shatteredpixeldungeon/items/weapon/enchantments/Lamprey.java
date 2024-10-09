b0f9fb752f9f7f6ecafa329ad4cc14640d3b6928
DoggingDog <chh1916308908@gmail.com>
2024-10-09 19:43:27 +0800

    # bug fix on 2024_10_09 ## Lamprey - fix null pointer exception

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/enchantments/Lamprey.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/enchantments/Lamprey.java
@@ -6,26 +6,29 @@ import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
 
-
 /*
     Class Name  : Lamprey
     Description : Weapon enchantment 4 Radish Dungeon
     Created by DoggingDog on 2024_10_06
 */
 public class Lamprey extends Weapon.Enchantment {
-    private static ItemSprite.Glowing LAMPREY = new ItemSprite.Glowing( 0x0000FF );
-    public int proc(Weapon weapon, Char attacker, Char defender, int damage ) {
-        if(damage > defender.HP && defender.isAlive()){
-            if(Dungeon.hero != null){
-                Wand wand = Dungeon.hero.belongings.getItem(Wand.class);
-                wand.partialCharge += Math.floor(procChanceMultiplier(attacker));
-                wand.partialCharge = Math.min(wand.maxCharges,wand.partialCharge);
-            }
+  private static ItemSprite.Glowing LAMPREY = new ItemSprite.Glowing(0x0000FF);
+
+  public int proc(Weapon weapon, Char attacker, Char defender, int damage) {
+    if (damage > defender.HP && defender.isAlive()) {
+      if (Dungeon.hero != null) {
+        Wand wand = Dungeon.hero.belongings.getItem(Wand.class);
+        if (wand != null) {
+          wand.partialCharge += Math.floor(procChanceMultiplier(attacker));
+          wand.partialCharge = Math.min(wand.maxCharges, wand.partialCharge);
         }
-        return damage;
+      }
     }
-    @Override
-    public ItemSprite.Glowing glowing() {
-        return LAMPREY;
-    }
+    return damage;
+  }
+
+  @Override
+  public ItemSprite.Glowing glowing() {
+    return LAMPREY;
+  }
 }
